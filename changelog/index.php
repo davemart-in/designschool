79275386c286b2f04516f9c5933adfadabcd4a40
davemart-in <growthdesigner@gmail.com>
2020-12-22 07:21:50 -0500

    Homepage and About page content

---
+++ changelog/index.php
@@ -0,0 +1,10 @@
+<?php $prefix = '../'; ?>
+<?php include $prefix . 'header.php'; ?>
+        <article>
+            <hgroup>
+                <h1>Changelog</h1>
+                <h2>A chronological list of updates to this site.</h2>
+            </hgroup>
+
+        </article>
+<?php include $prefix . 'footer.php'; ?>
---
+++ header.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Design Education Democratized</title>
+    <link rel="stylesheet" type="text/css" charset="utf-8"  media="screen, projection" href="/ds.css" />
+</head>
+<body>
+    <header>
+        <section>
+            <a href="/"><img src="/img/design-school.png"></a>
+            <h3>Product Designer</h3>
+            <h4>Education</h4>
+        </section>
+    </header>
+    <main>
+        <nav>
+            <div class="right">
+                <a href="/"<?php if ($_SERVER['REQUEST_URI'] == '/') { ?>style="font-weight: bold;"<?php } ?>>Home</a>
+                <a href="/about/"<?php if ($_SERVER['REQUEST_URI'] == '/about/') { ?>style="font-weight: bold;"<?php } ?>>About</a>
+                <a href="/changelog/"<?php if ($_SERVER['REQUEST_URI'] == '/changelog/') { ?>style="font-weight: bold;"<?php } ?>>Changelog</a>
+            </div>
+        </nav>
