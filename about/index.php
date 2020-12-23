691c3b53748e7f08d1a0110a55ce3209ce82afa6
davemart-in <growthdesigner@gmail.com>
2020-12-23 11:09:54 -0500

    Added changelog

--- about/index.php
+++ about/index.php
@@ -3,18 +3,34 @@
         <article>
             <hgroup>
                 <h1>About</h1>
-                <h2>The why behind this site</h2>
+                <h2>The "why" behind this site</h2>
             </hgroup>
             <p>Hiya! My name is <a href="https://davemart.in">Dave Martin</a>.</p>
-            <p>Over the years, I've been extremely blessed in my career as a product designer and I want to give back. The best way that I can think to do that is to share some of the things that I have learned.</p>
-            <h3>My vision</h3>
-            <ul>
-				<li><strong>Anyone can become a designer</strong> - With commitment and practice I strongly believe that ANYONE can become a product designer. Even you!</li>
-				<li><strong>Everyone is welcome</strong> - Enrollment is open to anyone. This school is and always will be free.</li>
-				<li><strong>Practical, not academic</strong> - The focus of the courses that I will share will be around teaching practical skills not academic theory.</li>
-				<li><strong>Avoid college debt</strong> - The system we have now where designers are forced to go thousands of dollars into debt before they can even start designing is antiquated. You <a href="https://davemart.in/dont-pay-for-design-school/">don’t need a degree to become a designer</a>.</li>
-				<li><strong>Lessons that come to you</strong> - Eventually, I'd like to build out a way for yout to add lessons to a queue and have your next lesson automatically emailed to you daily.</li>
-				<li><strong>Collaborative effort</strong> - I’d love to invite any designer who has benefitted from their career in design to give back to the next generation of designers by collaborating with me on this project. If you have any interest in getting involved, <a href="mailto:designpro@gmail.com">please reach out</a>.</li>
-			</ul>
+            <p>Over the years, I've been extremely blessed in my career as a product designer. I want to give back by sharing some of the things that I've learned.</p>
+            <h2>My vision</h2>
+            <p>
+                <strong>Anyone can become a designer</strong><br>
+                With commitment and practice I strongly believe that ANYONE can become a product designer. Even you!
+            </p>
+            <p>
+                <strong>Everyone is welcome</strong><br>
+                Enrollment is open to anyone. This school is and always will be free.
+            </p>
+            <p>
+                <strong>Practical, not academic</strong><br>
+                The focus of the courses that I will share will be around teaching practical skills not academic theory.
+            </p>
+            <p>
+                <strong>Avoid college debt</strong><br>
+                The system we have now where designers are forced to go thousands of dollars into debt before they can even start designing is antiquated. You <a href="https://davemart.in/dont-pay-for-design-school/">don’t need a degree to become a designer</a>.
+            </p>
+            <p>
+                <strong>Lessons that come to you</strong><br>
+                Eventually, I'd like to build out a way for yout to add lessons to a queue and have your next lesson automatically emailed to you daily.
+            </p>
+            <p>
+                <strong>Collaborative effort</strong><br>
+                I’d love to invite any designer who has benefitted from their career in design to give back to the next generation of designers by collaborating with me on this project. If you have any interest in getting involved, <a href="mailto:designpro@gmail.com">please reach out</a>.
+            </p>
         </article>
 <?php include $prefix . 'footer.php'; ?>
--- changelog/index.php
+++ changelog/index.php
@@ -3,8 +3,33 @@
         <article>
             <hgroup>
                 <h1>Changelog</h1>
-                <h2>A chronological list of updates to this site.</h2>
+                <h2>A list of updates to this site</h2>
             </hgroup>
-
+            <div class="changelog">
+            	<div class="day">
+            		<h3>Dec 23, 2020</h3>
+            		<div class="task">
+            			<span class="badge added">added</span>
+            			<div class="desc">
+            				<p>Changelog page</p>
+            			</div>
+            		</div>
+            	</div>
+            	<div class="day">
+            		<h3>Dec 22, 2020</h3>
+            		<div class="task">
+            			<span class="badge added">added</span>
+            			<div class="desc">
+            				<p>About page</p>
+            			</div>
+            		</div>
+            		<div class="task">
+            			<span class="badge added">added</span>
+            			<div class="desc">
+            				<p>Homepage</p>
+            			</div>
+            		</div>
+            	</div>
+            </div>
         </article>
 <?php include $prefix . 'footer.php'; ?>
