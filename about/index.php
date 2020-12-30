5559c5fc99f7c27789e8cb57b1ab920d585a060e
davemart-in <growthdesigner@gmail.com>
2020-12-30 09:24:09 -0500

    Moving html over to php

---
+++ about/index.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Design School - Product designer Education Democratized</title>
+    <link rel="stylesheet" type="text/css" charset="utf-8"  media="screen, projection" href="/ds.css" />
+</head>
+<body>
+    <div class="modal_backdrop">
+        <div class="modal"></div>
+    </div>
+    <header>
+        <section>
+            <a href="/"><img src="/img/design-school.png"></a>
+            <h3>Design School</h3>
+            <h4>Product Designer Education</h4>
+        </section>
+    </header>
+    <main>
+        <nav>
+            <div class="right">
+                <a href="/">Home</a>
+                <a href="/about/">About</a>
+                <a href="/changelog/">Changelog</a>
+                <a href="https://github.com/davemart-in/designschool/edit/main/index.html" class="edit btn">Edit</a>
+            </div>
+        </nav>
+        <article>
+            <hgroup>
+                <h1>About</h1>
+                <h2>Vision for this project</h2>
+            </hgroup>
+            <p>I'm <a href="https://davemart.in">Dave Martin</a>.</p>
+            <p>Over the years I've been extremely blessed by the opportunities I've received in my career as a product designer. As I move on to a different chpter of my life, I feel a strong desire to record the things that I've learned over the years. My hope is that this will serve both as a resource for up and coming designers but also a reference guide for my future self.</p>
+            <h2>Mission</h2>
+            <strong>Democratize design education</strong>
+            <h2>Principles</h2>
+            <p>The guiding principles for this project are to be:</p>
+            <ul>
+                <li>Inclusive</li>
+                <li>Iterative</li>
+                <li>Simple</li>
+                <li>Fun</li>
+                <li>Collaborative</li>
+            </ul>
+            <h2>Objectives</h2>
+            <p>
+                <strong>Anyone can become a designer</strong><br>
+                With commitment and practice I believe that ANYONE can become a product designer. Even you!
+            </p>
+            <p>
+                <strong>Everyone is welcome</strong><br>
+                Enrollment to this design school is open to anyone. This school is and always will be free.
+            </p>
+            <p>
+                <strong>Practical, not academic</strong><br>
+                I never went to design school. The focus of the courses that I will share will be much more around teaching practical skills than academic theory.
+            </p>
+            <p>
+                <strong>Avoid college debt</strong><br>
+                The system we have now where designers go thousands of dollars into debt before they can even start designing is antiquated. You <a href="https://davemart.in/dont-pay-for-design-school/">don’t need a degree to become a designer</a>.
+            </p>
+            <p>
+                <strong>Lessons that come to you</strong><br>
+                Eventually, I'd like to build out a way for you to add lessons to a queue and have your next lesson automatically emailed to you daily.
+            </p>
+            <p>
+                <strong>Translated to multiple languages</strong><br>
+                Eventually, I'd love to see these resources translated to as many languages as possible.
+            </p>
+            <p>
+                <strong>Strong community</strong><br>
+                Eventually, I'd like to build a community around this project, helping aspiring product designers connect and suppport each other.
+            </p>
+            <p>
+                <strong>Collaborative effort</strong><br>
+                I invite any designer who has seen similar blessings in their design career to give back to the next generation of designers by collaborating with me on this project. If you'd like to get involved, simply click the "Edit" button on any page.
+            </p>
+        </article>
+    </main>
+    
+    <script type="text/javascript" src="/ds.js"></script>
+</body>
+</html>
